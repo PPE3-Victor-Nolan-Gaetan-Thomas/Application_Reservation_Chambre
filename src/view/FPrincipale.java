b5f577117f4da56fb92107dac72eb3ca31e0409c
VictorLouvet <37446188+VictorLouvet@users.noreply.github.com>
2018-10-17 14:01:33 +0200

    Instantiation

--- src/view/FPrincipale.java
+++ src/view/FPrincipale.java
@@ -12,6 +12,9 @@ import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
 import com.toedter.calendar.JDateChooser;
+import javax.swing.JList;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class FPrincipale extends JFrame {
 
@@ -37,6 +40,27 @@ public class FPrincipale extends JFrame {
 			}
 		});
 	}
+	
+	//Instanciation de tout les composants
+	JLabel lblRservation = new JLabel("R\u00E9servation : ");
+	JLabel lblNom = new JLabel("Nom : ");
+	JLabel lblPrenom = new JLabel("Pr\u00E9nom : ");
+	JLabel lblNumRes = new JLabel("Num\u00E9ro r\u00E9servation : ");
+	JLabel lblDate = new JLabel("Date arriv\u00E9e : ");
+	JLabel lblNbJourRes = new JLabel("Dur\u00E9e s\u00E9jour : ");
+	JLabel lbl_reglement = new JLabel("Type de r\u00E9glement : ");
+	JLabel lblMail = new JLabel("Mail : ");
+	JLabel type_chambre = new JLabel("Type de chambre : ");
+	JDateChooser date = new JDateChooser();
+	JComboBox combo_type_chambre = new JComboBox();
+	JComboBox combo_reglement = new JComboBox();
+	JButton btnValider = new JButton("Valider");
+	JButton btnAnnuler = new JButton("Annuler");
+	JButton btnSupprimer = new JButton("Supprimer");
+	JList list = new JList();
+	
+	
+	//Fin d'instantiation des composants
 
 	/**
 	 * Create the frame.
@@ -50,12 +74,11 @@ public class FPrincipale extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblRservation = new JLabel("R\u00E9servation : ");
+		
 		lblRservation.setFont(new Font("Tahoma", Font.PLAIN, 38));
 		lblRservation.setBounds(12, 13, 235, 48);
 		contentPane.add(lblRservation);
 		
-		JLabel lblNom = new JLabel("Nom : ");
 		lblNom.setBounds(12, 110, 56, 16);
 		contentPane.add(lblNom);
 		
@@ -64,7 +87,6 @@ public class FPrincipale extends JFrame {
 		contentPane.add(txt_nom);
 		txt_nom.setColumns(10);
 		
-		JLabel lblPrenom = new JLabel("Pr\u00E9nom : ");
 		lblPrenom.setBounds(12, 142, 73, 16);
 		contentPane.add(lblPrenom);
 		
@@ -73,7 +95,6 @@ public class FPrincipale extends JFrame {
 		txt_prenom.setBounds(167, 136, 174, 22);
 		contentPane.add(txt_prenom);
 		
-		JLabel lblNumRes = new JLabel("Num\u00E9ro r\u00E9servation : ");
 		lblNumRes.setBounds(12, 174, 126, 16);
 		contentPane.add(lblNumRes);
 		
@@ -82,11 +103,9 @@ public class FPrincipale extends JFrame {
 		txt_num_res.setBounds(167, 168, 174, 22);
 		contentPane.add(txt_num_res);
 		
-		JLabel lblDate = new JLabel("Date arriv\u00E9e : ");
 		lblDate.setBounds(12, 203, 126, 16);
 		contentPane.add(lblDate);
 		
-		JLabel lblNbJourRes = new JLabel("Dur\u00E9e s\u00E9jour : ");
 		lblNbJourRes.setBounds(12, 235, 126, 16);
 		contentPane.add(lblNbJourRes);
 		
@@ -95,11 +114,9 @@ public class FPrincipale extends JFrame {
 		txt_nb_jour_res.setBounds(167, 232, 174, 22);
 		contentPane.add(txt_nb_jour_res);
 		
-		JLabel lbl_reglement = new JLabel("Type de r\u00E9glement : ");
 		lbl_reglement.setBounds(12, 334, 126, 16);
 		contentPane.add(lbl_reglement);
 		
-		JLabel lblMail = new JLabel("Mail : ");
 		lblMail.setBounds(12, 267, 126, 16);
 		contentPane.add(lblMail);
 		
@@ -108,29 +125,41 @@ public class FPrincipale extends JFrame {
 		txt_mail.setBounds(167, 264, 174, 22);
 		contentPane.add(txt_mail);
 		
-		JLabel type_chambre = new JLabel("Type de chambre : ");
 		type_chambre.setBounds(12, 302, 126, 16);
 		contentPane.add(type_chambre);
 		
-		JComboBox combo_type_chambre = new JComboBox();
+		contentPane.add(date);
+		date.setBounds(167,  203, 174, 22);
+		
 		combo_type_chambre.setBounds(167, 299, 174, 22);
 		contentPane.add(combo_type_chambre);
 		
-		JComboBox combo_reglement = new JComboBox();
 		combo_reglement.setBounds(167, 331, 174, 22);
 		contentPane.add(combo_reglement);
 		
-		JButton btnValider = new JButton("Valider");
 		btnValider.setBounds(12, 384, 126, 59);
 		contentPane.add(btnValider);
 		
-		JButton btnAnnuler = new JButton("Annuler");
+		btnAnnuler.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				txt_nom.setText(null);
+				txt_prenom.setText(null);
+				txt_num_res.setText(null);
+				txt_nb_jour_res.setText(null);
+				txt_mail.setText(null);
+				date.setDate(null);
+				combo_type_chambre.setSelectedItem(null);
+				combo_reglement.setSelectedItem(null);
+			}
+		});
 		btnAnnuler.setBounds(167, 384, 126, 59);
 		contentPane.add(btnAnnuler);
 		
-		JButton btnSupprimer = new JButton("Supprimer");
 		btnSupprimer.setBounds(321, 384, 126, 59);
 		contentPane.add(btnSupprimer);
+		
+		list.setBounds(497, 13, 363, 430);
+		contentPane.add(list);
 		
 		
 		
